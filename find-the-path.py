736f936b237209f2a47e3d9e34bdf74e80c8c1dc
Greg <85594736+muchas-gracias@users.noreply.github.com>
2025-04-11 13:22:14 -0500

    added functionality

--- find-the-path.py
+++ find-the-path.py
@@ -2,7 +2,7 @@
 class Circular:
     def __init__(self, items):
         self.lst = items
-        self.curr_idx = 1
+        self.curr_dir = 1
 
     def __getitem__(self, idx):
         item = (self.curr_idx + idx) % len(self.lst)
@@ -26,6 +26,9 @@ class Traverse:
     def __init__(self, grid):
         self.next = False
         self.end = False
+        self.set_current = False
+
+        self.pivot_points = {}
 
 
         self.cir = Circular([0,1,2,3])
@@ -34,8 +37,7 @@ class Traverse:
         self.grid = grid
         self.symbols = {"blocker": "x", "go": "o", "start": ">", "end": "<"}
         self.blockers = []
-        self.stuck = {}
-        self.stuck_count = 0
+
         self.current = (0, 0)
         self.directions = [
             lambda: self.move(-1, 0), #north
@@ -54,43 +56,72 @@ class Traverse:
 
         return True
 
-
-    def move(self, vertical, horizontal):
-
-        self.next = False
-
-        print(f" current is {self.current}")
-
-        # # setting  temp movement to the next projected move
-        movement = (self.current[0] + vertical, self.current[1] + horizontal)
-        print(f"after changing movement {movement}")
-
-        # if there isn't an  blocker
-        if movement not in self.blockers and self.check_if_in_bounds(movement):
+    def check_if_not_obstacle(self, move):
+        if move in self.blockers:
+            return False
+        return True
+            
+    def move(self, vert, horiz):
+        
+        
+        movement = (self.current[0] + vert, self.current[1] + horiz)
+        if self.set_current:
             self.current = movement
-            print(f"changing current {self.current}")
+            
+        if not self.check_if_in_bounds(movement) or not self.check_if_not_obstacle(movement):
+            return False
+        return True       
+     
+    def check_around(self, lst):
+        temp = []
+        
+        for each in lst:
+            if self.directions[each]():
+                temp.append(each)
+        
+        return temp
+            
+    def change_current_point(self, direction):
+             pass
+                
+            
+        
+    # def move(self, vertical, horizontal):
 
-        elif not self.check_if_in_bounds(movement):
-            self.next = True
-            print("out of bounds")
-            return
+        # self.next = False
 
-        else: # if there is an obstacle
-            if self.current not in self.stuck: # if obstacle not visited
+        # print(f" current is {self.current}")
 
-                self.stuck[self.current] = 1
+        # # # setting  temp movement to the next projected move
+        # movement = (self.current[0] + vertical, self.current[1] + horizontal)
+        # print(f"after changing movement {movement}")
 
-            else:
-                if self.stuck.get(self.current) == 3: # if obstacle visited 3x's
-                    self.blockers.append(self.current) # adding to blockers list
-                    del self.stuck[self.current]
+        # # if there isn't an  blocker
+        # if movement not in self.blockers and self.check_if_in_bounds(movement):
+        #     self.current = movement
+        #     print(f"changing current {self.current}")
 
-                else: # if obstacle hasn't been visited 3x's increment
-                    self.stuck[self.current] += 1
+        # elif not self.check_if_in_bounds(movement):
+        #     self.next = True
+        #     print("out of bounds")
+        #     return
 
-            self.next = True # move to the next direction
+        # else: # if there is an obstacle
+        #     if self.current not in self.stuck: # if obstacle not visited
 
-        return
+        #         self.stuck[self.current] = 1
+
+        #     else:
+        #         if self.stuck.get(self.current) == 3: # if obstacle visited 3x's
+        #             self.blockers.append(self.current) # adding to blockers list
+        #             del self.stuck[self.current]
+
+        #         else: # if obstacle hasn't been visited 3x's increment
+        #             self.stuck[self.current] += 1
+
+        #     self.next = True # move to the next direction
+
+        # return
 
 
     def find_start(self):
@@ -114,55 +145,60 @@ class Traverse:
         # for 4-11 each move evaluate 3 side and mark possibilities
         max_iter = len(self.directions)
         from_dir = 2
+        next_move = 0
+        prev_move = 0
 
         self.get_all_blockers()
         self.find_start()
         greg = 0
         nel = 0
-        self.cir.curr_idx = 1
+        
+        self.cir.curr_dir = 1 # starting direction east
+        g = 0
         while not self.end:
             if not self.cir.lst:
                 self.cir.fill_traveled()
 
-            #removing coming from directions
-            self.cir.remove_from_traveled(self.cir.__getitem__(from_dir))
-            print("++++++++++++")
-
             while self.cir.lst:
-                greg += 1
-                self.directions[self.cir.curr_idx]() #travel in the direction
-
-                if self.end:
+                g += 1
+                self.cir.lst = self.check_around(self.cir.lst) # checking in all 4 directions
+                print(f" \t\t\t\tcircle list {self.cir.lst}")
+                if len(self.cir.lst) > 1:
+                    next_move = self.cir.lst.pop(0)
+                    self.pivot_points[self.current] = {"possible": self.cir.lst[:], "next": next_move}
+                
+                
+                self.set_current = True
+                self.directions[next_move]()
+                print(f"next move {next_move}")
+                self.set_current = False
+                
+                print(f" current is {self.current}")
+                self.cir.lst.clear()
+                self.cir.fill_traveled()
+                
+                # check arount
+                
+                
+                
+                        
+                    
+                
+                if g == 8:
                     break
+            break
 
-                if self.next:
-                    print(f"curr direction ++ {self.cir.curr_idx}")
-                    print(self.cir.lst)
-                    self.cir.remove_from_traveled(self.cir.curr_idx) #removing curr direction
-                    print("gregory")
-                    self.cir.curr_idx = self.cir.lst.pop(0) # setting next direction in the list
-                    print(f"current direction {self.cir.curr_idx} and list is {self.cir.lst}")
-                    print(f"\t \t \t\t\tcoordinates now are {self.current}")
-                if greg == 500:
-                    break
-            nel += 1
-            if nel == 255:
-                break
+        print(f"pivot points {self.pivot_points}")
+        print(f"continued direction {self.cir.lst}")
 
 
 
-
-
-
-
-
-        print(self.stuck)
 
 def main():
 
     grid = [
     ['>', 'o', 'o', 'o', 'o', 'x', 'x', 'o'],
-    ['x', 'x', 'o', 'x', 'o', 'o', 'o', 'o'],
+    ['o', 'x', 'o', 'x', 'o', 'o', 'o', 'o'],
     ['x', 'x', 'x', 'x', 'o', 'x', 'x', 'o'],
     ['o', 'o', 'x', 'o', 'o', 'o', 'x', 'o'],
     ['x', 'o', 'o', 'x', 'x', 'x', 'x', 'o'],
